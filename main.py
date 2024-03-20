4fe47f25b97f582fe69a1593c5883565e0408b2f
seeifsalem <mohamedseifsalem@gmail.com>
2024-03-20 15:50:48 +0100

    tout marche inch

--- main.py
+++ main.py
@@ -3,36 +3,19 @@ from langchain import PromptTemplate
 from langchain.llms import OpenAI
 
 template = """
-    Below is an email that may be poorly worded.
-    Your goal is to:
-    - Properly format the email
-    - Convert the input text to a specified tone
-    - Convert the input text to a specified dialect
+You will be given text. This will be enclosed in triple backticks.
+You are a compliance checker for the registration of a medical device under the MDR regulation. 
+Check whether the given text by the user fulfills the requirements. If the Requirement is fulfilled, return TRUE. Jump a line and then congratulate the user on getting their MDR certification.
+Otherwise return FALSE, jump a line, explain what is wrong, and rewrite the text according to correct standards.
 
-    Here are some examples different Tones:
-    - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you.
-    - Informal: Went to Barcelona for the weekend. Lots to tell you.  
+'''{description}'''
 
-    Here are some examples of words in different dialects:
-    - American: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
-    - British: chips, candyfloss, flag, rubbish, biscuit, green fingers, car park, trousers, windscreen
-
-    Example Sentences from each dialect:
-    - American: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
-    - British: Well, I popped down to the local shop just the other day to pick up a few bits and bobs. As I was perusing the aisles, I noticed that they were fresh out of biscuits, which was a bit of a disappointment, as I do love a good cuppa with a biscuit or two.
-
-    Please start the email with a warm introduction. Add the introduction if you need to.
-    
-    Below is the email, tone, and dialect:
-    TONE: {tone}
-    DIALECT: {dialect}
-    EMAIL: {email}
-    
-    YOUR {dialect} RESPONSE:
+Result:
 """
 
+
 prompt = PromptTemplate(
-    input_variables=["tone", "dialect", "email"],
+    input_variables=["description"],
     template=template,
 )
 
@@ -42,66 +25,40 @@ def load_LLM(openai_api_key):
     llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
     return llm
 
-st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
-st.header("Globalize Text")
+st.set_page_config(page_title="MedRegPro", page_icon=":robot:")
+st.header("MedRegPro checker")
 
-col1, col2 = st.columns(2)
-
-with col1:
-    st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
-                will help you improve your email skills by converting your emails into a more professional format. This tool \
-                is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
-                [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
-
-with col2:
-    st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
-
-st.markdown("## Enter Your Email To Convert")
+st.markdown("Requirement: Provide a general description of the device, including its intended purpose, intended users (e.g., healthcare professionals, patients), and the medical conditions, diseases, or medical states it's meant to diagnose, treat, monitor, prevent, or alleviate. It should also cover the device's mode of action and the principle of operation")
 
 def get_api_key():
-    input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input")
-    return input_text
+    return "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"
 
 openai_api_key = get_api_key()
 
-col1, col2 = st.columns(2)
-with col1:
-    option_tone = st.selectbox(
-        'Which tone would you like your email to have?',
-        ('Formal', 'Informal'))
-    
-with col2:
-    option_dialect = st.selectbox(
-        'Which English Dialect would you like?',
-        ('American', 'British'))
-
 def get_text():
-    input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
+    input_text = st.text_area(label="Device Description", label_visibility='collapsed', placeholder="Your description...", key="input")
     return input_text
 
-email_input = get_text()
+input = get_text()
 
-if len(email_input.split(" ")) > 700:
-    st.write("Please enter a shorter email. The maximum length is 700 words.")
-    st.stop()
 
-def update_text_with_example():
-    print ("in updated")
-    st.session_state.email_input = "Sally I am starts work at yours monday from dave"
-
-st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
-
-st.markdown("### Your Converted Email:")
-
-if email_input:
-    if not openai_api_key:
-        st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
-        st.stop()
-
+def get_feedback():
     llm = load_LLM(openai_api_key=openai_api_key)
 
-    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+    prompt_with_description = prompt.format(description=input)
 
-    formatted_email = llm(prompt_with_email)
+    formatted_description = llm(prompt_with_description)
 
-    st.write(formatted_email)
+    st.write("## Your Feedback")
+
+    st.write(formatted_description)
+
+if st.button("*Check*", type='primary', help="Just click bro come on."):
+       
+    with st.spinner("Checking MDR Regulations... please wait..."):
+       
+
+       
+        get_feedback()
+
+
